dd3aad4dea5c1d026d08512eb5a2f2947f22e61d
toshkaraf <user@example.com>
2026-01-27 17:07:28 +0100

    Обновить web-билд после фикса ошибок

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "431259092"
+    serviceWorkerVersion: "741903631"
   }
 });
